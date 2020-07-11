5bb4004829abf88e0bb919a3bf409c615bfe8d0a
skchauhan <skrajput42@gmail.com>
2020-07-11 23:46:56 +0530

    inital commit

---
+++ protected/components/Helper.php
@@ -0,0 +1,15 @@
+<?php 
+class Helper {
+	public static function tap()
+	{
+		echo "Custom helper function tap";
+	}
+
+	public function test()
+	{
+		echo "Custom helper function test";
+	}
+}
+
+ ?>
+
---
+++ protected/extensions/components/MyClass.php
@@ -0,0 +1,10 @@
+<?php 
+
+class MyClass extends CApplicationComponent {
+
+      public function get_my_info() {
+      $value = '1';
+          return $value;
+     }
+
+    }
